8e62319f9494cfc7c4b8d5d401afbbefbeb5274f
predator1245 <jashshah37@yahoo.com>
2022-01-22 21:22:45 +0100

    Update portal behaviour

--- Assets/Scripts/PortalBehaviour.cs
+++ Assets/Scripts/PortalBehaviour.cs
@@ -29,12 +29,9 @@ public class PortalBehaviour : MonoBehaviour
         Debug.Log(col.transform.tag);
         if (col.transform.CompareTag("Player") && m_otherPortal != null)
         {
-            Debug.Log(m_otherPortal.transform.position);
-            //Reposition the player in front of another portal
             col.gameObject.GetComponent<CharacterController>().enabled = false;
             col.gameObject.transform.position = m_otherPortal.transform.position - Quaternion.Euler(0, 90, 0) * m_otherPortal.transform.forward * 1.5f;
             col.gameObject.transform.forward = m_otherPortal.transform.up;
-            // col.gameObject.transform.rotation = Quaternion.
             col.gameObject.GetComponent<CharacterController>().enabled = true;
         }
     }
